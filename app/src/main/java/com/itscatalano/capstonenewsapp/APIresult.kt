02fb77575ad5625420144376bf69c00f9daf18cc
Anthony Catalano <Anthony.Catalano@its.ny.gov>
2022-08-26 12:02:38 -0400

    setup data binding add data classes
    setup databinding and add three data classes one for the APIresult, Article and Source. The APIresult will hold a list of Articles

---
+++ app/src/main/java/com/itscatalano/capstonenewsapp/APIresult.kt
@@ -0,0 +1,10 @@
+package com.itscatalano.capstonenewsapp
+
+/***
+ * Author: Anthony Catalano
+ * Will need this eventually when we connect to the API
+ * and based on the status we can display how many articles there are
+ * and push the results of the articles in a a list of Article
+ *
+ */
+data class APIresult(val articles: List<Article>, val status: String, val totalResults: Int)
--- app/src/main/java/com/itscatalano/capstonenewsapp/MainActivity.kt
+++ app/src/main/java/com/itscatalano/capstonenewsapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.itscatalano.capstonenewsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.itscatalano.capstonenewsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        // set content view after binding
+        val view = binding.root
+        setContentView(view)
+
+        val mainGroup = binding.mainGroup
     }
 }
